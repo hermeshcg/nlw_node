00ae5630d8af676a54f5a27ef6d3376aa08bdc21
hermeshcg <hermescaretta@gmail.com>
2021-08-10 11:43:49 -0300

    aula 1

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  return res.json({
+    message: ':)',
+  });
+});
+
+app.post('/test-post', (request, response) => {
+  return response.send('Olá NLW método POST');
+});
+
 app.listen(3001, () => console.log('Server is running'));
